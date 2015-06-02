73d044e1e9d32fe1bace8c0c386c76707bf84628
sor16 <solviro@gmail.com>
2015-06-02 11:05:05 +0000

    figure 2 finished in posteriormyndir.R

--- posterirormyndir.R
+++ posterirormyndir.R
@@ -14,5 +14,12 @@ quantmatrix=t(cbind(quantyp1,quantyp2,quantyp3,quantyp4))
 prctile=t(apply(quantmatrix, 2, quantile, probs = c(0.025, 0.975),  na.rm = TRUE))
 data$lower=prctile[,1]
 data$upper=prctile[,2]
-g=ggplot(data,aes(l_m,fit))+geom_line()+theme_bw()+geom_point(data=data,mapping=aes(l_m,Q))+geom_line(mapping=aes(l_m,lower))+
-geom_line(mapping=aes(l_m,upper))
+g=ggplot(data)+geom_line(mapping=aes(l_m,fit))+theme_bw()+geom_point(mapping=aes(l_m,Q))+geom_line(mapping=aes(l_m,lower),linetype="dashed")+
+geom_line(mapping=aes(l_m,upper),linetype="dashed")
+
+##figure 2
+data$resid=(RC$y-X_m%*%mu)/sqrt(exp(t_m[2]))
+
+f=ggplot(data)+geom_point(aes(l_m,resid))+geom_abline(intercept = 0, slope = 0)+
+  geom_abline(intercept = 2, slope = 0,linetype="dashed")+geom_abline(intercept = -2, slope = 0,linetype="dashed")+ylim(-4,4)
+
