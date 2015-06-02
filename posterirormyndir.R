4071803281e21874b2c2ab43431d31dca3744e66
sor16 <solviro@gmail.com>
2015-06-02 14:31:08 +0000

    figure added with resid plot real scale

--- posterirormyndir.R
+++ posterirormyndir.R
@@ -16,11 +16,19 @@ rclog=ggplot(data)+geom_line(mapping=aes(l_m,fit))+theme_bw()+geom_point(mapping
 geom_line(mapping=aes(l_m,upper),linetype="dashed")
 
 ##figure 2 residual log scale
-data$resid=(RC$y-X_m%*%mu)/sqrt(exp(t_m[2]))
+data$residlog=(expRC$y-X_m%*%mu)/sqrt(exp(t_m[2,]))
 
-rcleif=ggplot(data)+geom_point(aes(l_m,resid))+theme_bw()+geom_abline(intercept = 0, slope = 0)+
-  geom_abline(intercept = 2, slope = 0,linetype="dashed")+geom_abline(intercept = -2, slope = 0,linetype="dashed")+ylim(-4,4)
+rcleiflog=ggplot(data)+geom_point(aes(l_m,residlog),color="red")+theme_bw()+geom_abline(intercept = 0, slope = 0)+
+  geom_abline(intercept = 2, slope = 0,linetype="dashed")+geom_abline(intercept = -2, slope = 0,linetype="dashed")+ylim(-4,4)+
+  ylab(expression(epsilon[i]))
 
 ##figure 6 rating curve real scale with conf interval
-rcreal=ggplot(data)+theme_bw()+geom_point(aes(exp(Q),W))+geom_line(aes(exp(fit),W))+geom_line(aes(exp(lower),W),linetype="dashed")+geom_line(aes(exp(upper),W,linetype="dashed"))
+rcreal=ggplot(data)+theme_bw()+geom_point(aes(exp(Q),W,col))+geom_line(aes(exp(fit),W))+geom_line(aes(exp(lower),W),linetype="dashed")+
+  geom_line(aes(exp(upper),W),linetype="dashed")
+
+##residual realscale
+data$residraun=(exp(RC$y)-exp(X_m%*%mu))/sqrt(exp(t_m[2,]))
+rcleifraun=ggplot(data)+geom_point(aes(exp(l_m),residraun),color="red")+theme_bw()+geom_abline(intercept = 0, slope = 0)+
+  geom_abline(intercept = exp(2), slope = 0,linetype="dashed")+geom_abline(intercept = -exp(2), slope = 0,linetype="dashed")+
+  ylim(-8,8)+ylab(expression(epsilon[i]))
 
